a75ccd60a4c21d6b515b7b0602f6b5e69ea11f39
Gustav <gustav@invoke.se>
2014-10-10 13:50:19 +0200

    Add Payback() helper method

--- lib/payback.rb
+++ lib/payback.rb
@@ -1,5 +1,29 @@
 require "payback/version"
+require 'payback/conversion'
+
+require 'open-uri'
+require 'base64'
+require 'excon'
+require 'json'
+require 'csv'
 
 module Payback
-  # Your code goes here...
+
+  def self.networks
+    @@networks ||= {}
+  end
+
+  def self.register(network_klass)
+    networks[network_klass.identifier] = network_klass
+  end
+
+end
+
+require 'payback/networks'
+
+def Payback(network_name, options = {})
+  klass = Payback.networks.fetch(network_name.to_s) do |missing_name|
+    raise "Network named #{missing_name} does not exist!"
+  end
+  klass.new(options)
 end
