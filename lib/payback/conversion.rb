c97a9c557ad895b9723140205b52bc82b27d6c21
Gustav <gustav@invoke.se>
2014-10-10 13:49:16 +0200

    Add conversion

---
+++ lib/payback/conversion.rb
@@ -0,0 +1,39 @@
+module Payback
+  class Conversion
+
+    # UID
+    # EPI
+    # Commission
+    # Currency
+    # Network
+    # Channel
+    # Program
+    # Status
+    # Timestamp
+
+    ATTRIBUTES = %w(uid epi commission currency network
+      channel program status timestamp)
+
+    attr_accessor *ATTRIBUTES
+
+    def initialize(attrs = {})
+      attrs.each do |key, value|
+        public_send("#{key}=", value)
+      end
+    end
+
+    # TODO: Add GUID method?
+    # def guid
+    #   "#{network}-#{uid}"
+    # end
+
+    def attributes
+      Hash[ATTRIBUTES.map { |key| [key, send(key)] }]
+    end
+
+    def valid?
+      attributes.values.all? { |x| !x.to_s.strip.empty? }
+    end
+
+  end
+end
