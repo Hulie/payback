42f91d5db8c3fba8b27b84b7590857d700b83103
Gustav <gustav@invoke.se>
2015-08-21 11:56:17 +0200

    Remove affiliator

--- lib/payback/networks.rb
+++ lib/payback/networks.rb
@@ -2,7 +2,6 @@
 require 'payback/networks/base'
 require 'payback/networks/adrecord'
 require 'payback/networks/adtraction'
-require 'payback/networks/affiliator'
 require 'payback/networks/cj'
 require 'payback/networks/double'
 require 'payback/networks/partner_ads'
