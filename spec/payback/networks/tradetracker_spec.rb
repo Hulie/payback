ae74ffd3db46a57275a3e64198caa09ce9291a39
Gustav <gustav@invoke.se>
2017-02-02 15:15:30 +0100

    Update tradetracker cassette with dummy data

--- spec/payback/networks/tradetracker_spec.rb
+++ spec/payback/networks/tradetracker_spec.rb
@@ -9,14 +9,14 @@ describe Payback::Networks::Tradetracker do
       instance = subject.new
       records = instance.between('2014-10-10', '2014-10-11')
       records.first.tap do |record|
-        record.uid.must_equal "1155747527"
+        record.uid.must_equal "123456789"
         record.commission.must_equal 21.392
         record.epi.must_equal ''
         record.currency.must_equal 'SEK'
         record.network.must_equal 'tradetracker'
-        record.channel.must_equal 'SEM Sweden'
+        record.channel.must_equal 'example.com'
         record.timestamp.must_equal Time.parse('2014-10-10 10:18:34 +0200')
-        record.program.must_equal 'Shaver.se'
+        record.program.must_equal 'foobar.com'
       end
     end
   end
